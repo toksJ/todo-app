bb65870295366cd9830e3aa5e1169cc73d924f69
TJ <tokajab@gmail.com>
2023-08-04 19:54:53 +0300

    Initial Commit

---
+++ todo app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo app
+//
+//  Created by TokaJaber on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
